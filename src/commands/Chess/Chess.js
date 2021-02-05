94c95f8755ebcc53d705b8c9b307ee8f37c6e55b
Anthony Gorski <anthony.developpeurweb@gmail.com>
2021-02-05 18:00:26 +0100

    final refacto done

--- src/commands/Chess/Chess.js
+++ src/commands/Chess/Chess.js
@@ -22,22 +22,36 @@ module.exports = class Btc extends (
 			.then(function (results) {
 				const acct = results[0].data;
 				const stats = results[1].data;
+				const finalResult = [];
 
-				result.map((word) => {
-					console.log(word);
-				});
-
+				/** l'api donne pour chaque partie le prefix chess_ */
 				const pattern = "chess_";
+				/** Je filtre le pattern et je récupère les infos  */
 				const result = Object.keys(stats).filter((str) => {
 					return str.includes(pattern);
 				});
 
-				console.log(result);
-				message.channel.send("coco");
-				console.log(messagePush);
+				/** boucle qui permet de push les infos des parties */
+				for (let i = 0; i < result.length; i++) {
+					let message = `> Partie ${result[i].split("_")[1]} 
+- Gagnant = ${stats[result[i]].record.win}
+- Perdant = ${stats[result[i]].record.loss}
+- Null = ${stats[result[i]].record.draw} \r\n`;
+					/** Je push chaque message dans mon array finalResult */
+					finalResult.push(message);
+				}
+
+				message.channel.send(
+					`Voici les stats pour le joueur **${
+						acct.username
+					}** (joueur ${acct.status}) \r\n${finalResult.join(" ")}`
+				);
 			})
-			.catch(() => {
-				message.reply(`Désolé, je ne connais pas le joueur ${args[1]}`);
+			.catch((err) => {
+				console.log(err);
+				message.reply(
+					`Désolé, je ne connais pas le joueur **${args[1]}**`
+				);
 			});
 	}
 };
